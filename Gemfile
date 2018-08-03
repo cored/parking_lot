da041232a058e99c4d898178ab7225810279bf4c
Rafael George <george.rafael@gmail.com>
2018-08-03 11:02:37 -0400

    Add examples to return proper structure to represent a parking lot

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-group :test do
+group :test, :development do
   gem "rspec"
+  gem "pry"
 end
--- spec/lib/parking_lot_spec.rb
+++ spec/lib/parking_lot_spec.rb
@@ -1,27 +1,86 @@
 require "spec_helper"
 
-module ParkingLot
+class ParkingLot
   NoAvailableParkingSpot = Class.new(StandardError)
 
-  def self.find_parking_spot(vehicle:)
-
-    raise NoAvailableParkingSpot
+  def self.parking_spots
+    ("A".."Z").each.with_object({}) do |lot_name, parking_spot|
+      parking_spot[lot_name] = {size: 20, assigned: nil}
+    end
   end
+
+  def self.find_parking_spot(vehicles:)
+    new(parking_spots: parking_spots).call(vehicles: vehicles)
+  end
+
+  def initialize(parking_spots:)
+    @parking_spots = parking_spots
+  end
+
+  def call(vehicles:)
+    assign_vehicles_to_available_spots_for(vehicles)
+    assigned_parking_spots
+  end
+
+  private
+
+  def assign_vehicles_to_available_spots_for(vehicles)
+    vehicles.map do |vehicle|
+      available_spot = available_parking_spot_for(unassigned_parking_spots, vehicle)
+      parking_spots[available_spot.first][:assigned] = vehicle
+    end
+  end
+
+  def unassigned_parking_spots
+    parking_spots.select do |name, spot|
+      spot[:assigned].nil?
+    end
+  end
+
+  def available_parking_spot_for(spots, vehicle)
+    spots.find do |name, spot|
+      spot[:size] >= vehicle[:size]
+    end
+  end
+
+  def assigned_parking_spots
+    parking_spots.select do |name, spot|
+      spot[:assigned]
+    end
+  end
+
+  attr_reader :parking_spots
 end
 
 describe ParkingLot do
   describe ".find_parking_spot" do
     context "when looking for an spot for a vehicle" do
-      it "returns no parking spot available error for vehicle without info" do
-        expect{
-          ParkingLot.find_parking_spot(vehicle: {})
-        }.to raise_error(described_class::NoAvailableParkingSpot)
-      end
+      # it "returns no parking spot available error for vehicle without info" do
+      #   expect{
+      #     ParkingLot.find_parking_spot(vehicles: [{}])
+      #   }.to raise_error(described_class::NoAvailableParkingSpot)
+      # end
 
       it "returns the first parking spot in which a vehicle can park if any" do
         expect(
-          ParkingLot.find_parking_spot(vehicle: {brand: "Dodge", size: 15, price: 180})
-        ).to eql({size: 50, name: "A", assigned: {brand: "Dodge", size: 15, price: 180}})
+          ParkingLot.find_parking_spot(vehicles: [{brand: "Dodge", size: 15, price: 180}])
+        ).to eql({"A" => {size: 20, assigned: {brand: "Dodge", size: 15, price: 180}}})
+      end
+
+      it "returns the parking spots for more than one vehicle" do
+        vehicles =[
+          {brand: "Dodge", size: 15, price: 180},
+          {brand: "Mercedes", size: 10, price: 50},
+          {brand: "Toyota", size: 20, price: 50},
+        ]
+
+        expect(
+          ParkingLot.find_parking_spot(vehicles: vehicles)
+        ).to eql({
+          "A" => {size: 20, assigned: {brand: "Dodge", size: 15, price: 180}},
+          "B"=> {:size=>20, assigned: {:brand=>"Mercedes", :size=>10, :price=>50}},
+          "C"=> {:size=>20, assigned: {:brand=>"Toyota", :size=>20, :price=>50}}
+        })
       end
     end
   end
